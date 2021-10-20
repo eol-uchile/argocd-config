3e3adb15de57d76e163f9c1391fa19709d2c37cf
Dario Andhael <spam.dario@gmail.com>
2021-10-20 10:27:40 -0300

    Update service backup images

---
+++ service-backup/secret_updates/gensecret.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export EDX_NAMESPACE=service-backup
+
+kubectl -n $EDX_NAMESPACE create secret generic common --dry-run=client --from-env-file=./common.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../secrets/common.yaml
+kubectl -n $EDX_NAMESPACE create secret generic metabase --dry-run=client --from-env-file=./metabase.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../secrets/metabase.yaml
+kubectl -n $EDX_NAMESPACE create secret generic otrs --dry-run=client --from-env-file=./otrs.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../secrets/otrs.yaml
+kubectl -n $EDX_NAMESPACE create secret generic graylog --dry-run=client --from-env-file=./graylog.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../secrets/graylog.yaml
