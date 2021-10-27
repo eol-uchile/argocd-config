5a5797f11c8955e533a8a0fae2c03683d77b084f
Dario Andhael <spam.dario@gmail.com>
2021-10-27 16:16:42 -0300

    Keep backups at node3 until the clean job deletion

--- edx-backup/secret_updates/pull.sh
+++ edx-backup/secret_updates/pull.sh
@@ -4,6 +4,7 @@ set -e
 export EDX_NAMESPACE=edx-backup
 
 # configs
+kubectl -n $EDX_NAMESPACE get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
 kubectl -n $EDX_NAMESPACE get secret uabierta-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > uabierta.env
 kubectl -n $EDX_NAMESPACE get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol.env
 kubectl -n $EDX_NAMESPACE get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx.env
