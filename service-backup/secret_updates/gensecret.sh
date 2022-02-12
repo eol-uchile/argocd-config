7349ae6bf4b07d08f8f3d670d7cf9b4e26842932
Felipe <github@espinoza.dev>
2022-02-12 13:50:54 +0000

    Adding NAS_ENCRYPTED_FILE_KEY

--- service-backup/secret_updates/gensecret.sh
+++ service-backup/secret_updates/gensecret.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+kubectl config use-context eol
 export EDX_NAMESPACE=service-backup
 
 kubectl -n $EDX_NAMESPACE create secret generic common --dry-run=client --from-env-file=./common.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../secrets/common.yaml
