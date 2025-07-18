c793a4eb1398afcd3535efa5d5f94da613229640
fsologureng <fsologur@uchile.cl>
2025-07-18 14:26:50 -0400

    chore: Implements new ArgoCD instance

--- secrets/argocd-ingress/gensecret.sh
+++ secrets/argocd-ingress/gensecret.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl config use-context eol
+kubectl config use-context oeol
 
 kubectl create secret generic basic-auth --dry-run=client --from-file=auth=./basic_auth -o yaml | kubeseal --scope cluster-wide --controller-namespace sealed-secrets -o yaml > ../../argocd-ingress/secret.yaml
