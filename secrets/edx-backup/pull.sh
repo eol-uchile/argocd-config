ef8dbbe7046c2e59b6cd2cc7b0fa7f65e48b73ec
fsologureng <fsologur@uchile.cl>
2025-08-22 16:46:37 -0400

    chore(edx-backup): remove unused uabierta bkp jobs

--- secrets/edx-backup/pull.sh
+++ secrets/edx-backup/pull.sh
@@ -9,5 +9,4 @@ kubectl -n $EDX_NAMESPACE get secret common-config -o json | jq -r '.data | keys
 kubectl -n $EDX_NAMESPACE get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
 kubectl -n $EDX_NAMESPACE get secret norteamericano-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > norteamericano-config.env
 kubectl -n $EDX_NAMESPACE get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
-kubectl -n $EDX_NAMESPACE get secret uabierta-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > uabierta-config.env
 kubectl -n $EDX_NAMESPACE get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
