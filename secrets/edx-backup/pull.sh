7c7925da4f69a12cb1ec82dbe3123b481def43ae
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-09-05 15:52:45 -0400

    chore(redfid): update name & path references

--- secrets/edx-backup/pull.sh
+++ secrets/edx-backup/pull.sh
@@ -7,7 +7,7 @@ export EDX_NAMESPACE=edx-backup
 # configs
 kubectl -n $EDX_NAMESPACE get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
 kubectl -n $EDX_NAMESPACE get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
-kubectl -n $EDX_NAMESPACE get secret norteamericano-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > norteamericano-config.env
+kubectl -n $EDX_NAMESPACE get secret redfid-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > redfid-config.env
 kubectl -n $EDX_NAMESPACE get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
 kubectl -n $EDX_NAMESPACE get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
 
