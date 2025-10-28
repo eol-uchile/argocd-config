1b95cc580cfafec2066b6f022ce20527a9ce1cf2
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-10-28 15:40:26 -0300

    chore(znuny): re-label secret management scripts to oeol context

--- secrets/znuny/gensecret.sh
+++ secrets/znuny/gensecret.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl config use-context eol
-export EDX_NAMESPACE=otrs
-
-kubectl -n $EDX_NAMESPACE create secret generic otrs --dry-run=client --from-env-file=./otrs.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../otrs/secret.yaml
+kubectl --context oeol -n znuny create secret generic znuny --dry-run=client --from-env-file=./znuny.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../znuny/secret.yaml
