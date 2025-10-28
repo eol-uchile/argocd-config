44c1693158a9269aad6576da142a80c9f75918ba
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-10-28 15:40:26 -0300

    chore(znuny): re-label secret management scripts to oeol context

--- secrets/znuny/pull.sh
+++ secrets/znuny/pull.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl config use-context eol
-export EDX_NAMESPACE=otrs
-
 # configs
-kubectl -n $EDX_NAMESPACE get secret otrs -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > otrs.env
+kubectl --context oeol -n znuny get secret znuny -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > znuny.env
