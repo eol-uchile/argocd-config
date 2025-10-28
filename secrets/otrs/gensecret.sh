5f74e132daaab05f17273650c90c8bfe4abedf03
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-10-28 15:40:26 -0300

    chore(znuny): update secret management scripts to oeol context

--- secrets/otrs/gensecret.sh
+++ secrets/otrs/gensecret.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl config use-context eol
-export EDX_NAMESPACE=otrs
-
-kubectl -n $EDX_NAMESPACE create secret generic otrs --dry-run=client --from-env-file=./otrs.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../otrs/secret.yaml
+kubectl --context oeol -n otrs create secret generic otrs --dry-run=client --from-env-file=./otrs.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../otrs/secret.yaml
