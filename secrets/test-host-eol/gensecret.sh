2ef8bb0290bcc4363edde3de2835e378148edc4c
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-11-11 18:20:40 -0300

    chore(test-host-eol): add kubeseal context in secret generation

--- secrets/test-host-eol/gensecret.sh
+++ secrets/test-host-eol/gensecret.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl --context oeol -n test-eol create secret generic s3-config --dry-run=client --from-env-file=./s3-settings.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../test-host-eol/secrets/s3-settings.yaml
+kubectl --context oeol -n test-eol create secret generic s3-config --dry-run=client --from-env-file=./s3-settings.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../test-host-eol/secrets/s3-settings.yaml
