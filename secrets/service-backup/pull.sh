59000c80f248db5982ab54f0d16fbed12f93e49f
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-11-12 10:29:31 -0300

    chore(service-backup): update sealed secret & scripts to oeol context

--- secrets/service-backup/pull.sh
+++ secrets/service-backup/pull.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl config use-context eol
-export EDX_NAMESPACE=service-backup
-
 # configs
-kubectl -n $EDX_NAMESPACE get secret common -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
-kubectl -n $EDX_NAMESPACE get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > metabase.env
-kubectl -n $EDX_NAMESPACE get secret otrs -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > otrs.env
+kubectl --context oeol -n service-backup get secret common -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
+kubectl --context oeol -n service-backup get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > metabase.env
+kubectl --context oeol -n service-backup get secret znuny -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > znuny.env
