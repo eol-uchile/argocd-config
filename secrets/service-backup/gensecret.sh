1bbbea1b6e57b4bf83d8459dbad53913833c76e2
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-11-13 11:35:33 -0300

    chore(service-backup): remove graylog backup from service-backup

--- secrets/service-backup/gensecret.sh
+++ secrets/service-backup/gensecret.sh
@@ -6,4 +6,3 @@ export EDX_NAMESPACE=service-backup
 kubectl -n $EDX_NAMESPACE create secret generic common --dry-run=client --from-env-file=./common.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../service-backup/secrets/common.yaml
 kubectl -n $EDX_NAMESPACE create secret generic metabase --dry-run=client --from-env-file=./metabase.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../service-backup/secrets/metabase.yaml
 kubectl -n $EDX_NAMESPACE create secret generic otrs --dry-run=client --from-env-file=./otrs.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../service-backup/secrets/otrs.yaml
-kubectl -n $EDX_NAMESPACE create secret generic graylog --dry-run=client --from-env-file=./graylog.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../service-backup/secrets/graylog.yaml
--- secrets/service-backup/pull.sh
+++ secrets/service-backup/pull.sh
@@ -8,4 +8,3 @@ export EDX_NAMESPACE=service-backup
 kubectl -n $EDX_NAMESPACE get secret common -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
 kubectl -n $EDX_NAMESPACE get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > metabase.env
 kubectl -n $EDX_NAMESPACE get secret otrs -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > otrs.env
-kubectl -n $EDX_NAMESPACE get secret graylog -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > graylog.env
