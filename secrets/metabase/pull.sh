91bcc57ab107b7d51dd43bdc28e97cdb2f6007d6
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-11-18 11:47:30 -0300

    chore(metabase): update sealed secret & scripts to oeol context

--- secrets/metabase/pull.sh
+++ secrets/metabase/pull.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl config use-context eol
-export EDX_NAMESPACE=metabase
-
 # configs
-kubectl -n $EDX_NAMESPACE get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > metabase.env
-kubectl -n $EDX_NAMESPACE get secret basic-auth -o jsonpath="{.data['auth']}" | base64 -d > basic_auth
+kubectl --context oeol -n metabase get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > /home/vicente/Documentos/repo/argocd-config/metabase.env
+kubectl --context oeol -n metabase get secret basic-auth -o jsonpath="{.data['auth']}" | base64 -d > /home/vicente/Documentos/repo/argocd-config/basic_auth
