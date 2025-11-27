9928bb93efa1e19a6d5b8982fef7c4c0f7985502
Vicente Astorga <vicente.astorga.dev@gmail.com>
2025-11-27 11:15:07 -0300

    fix(metabase): update secrets script path

--- secrets/metabase/gensecret.sh
+++ secrets/metabase/gensecret.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kubectl --context oeol -n metabase create secret generic metabase --dry-run=client --from-env-file=/home/vicente/Documentos/repo/argocd-config/metabase.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > /home/vicente/Documentos/repo/argocd-config/metabase/secrets.yaml
-kubectl --context oeol -n metabase create secret generic basic-auth --dry-run=client --from-file=auth=/home/vicente/Documentos/repo/argocd-config/basic_auth -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > /home/vicente/Documentos/repo/argocd-config/metabase/access.yaml
+kubectl --context oeol -n metabase create secret generic metabase --dry-run=client --from-env-file=./metabase.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../metabase/secrets.yaml
+kubectl --context oeol -n metabase create secret generic basic-auth --dry-run=client --from-file=auth=./basic_auth -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../metabase/access.yaml
