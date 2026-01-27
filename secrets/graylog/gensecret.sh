d7067417875865491a0f135865a72ea5b093f86a
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-01-27 12:56:34 -0300

    chore(graylog): remove application, configs & secrets

--- secrets/graylog/gensecret.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl config use-context eol
-export EDX_NAMESPACE=graylog
-
-kubectl -n $EDX_NAMESPACE create secret generic graylog --dry-run=client --from-env-file=./graylog.env -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../secrets/graylog/graylog.yaml
-kubectl -n $EDX_NAMESPACE create secret generic graylog-ca --dry-run=client --from-file=client.cert=./client.cert --from-file=server.cert=./server.cert --from-file=server.key=./server.key -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../graylog/secrets/graylog-ca.yaml
-kubectl -n $EDX_NAMESPACE create secret generic vector-cert --dry-run=client --from-file=client.cert=./client.cert --from-file=server.cert=./server.cert --from-file=client.key=./client.key -o yaml | kubeseal --controller-namespace sealed-secrets -o yaml > ../../graylog/secrets/vector-cert.yaml
