88229c73e8f555c30e06b536e8122b1cc3859c67
fsologureng <fsologur@uchile.cl>
2026-03-03 12:05:49 -0300

    refactor(moodle-vlabx): change secret mgmt scripts
    Put context & namespace in each kubectl call of gen & pull secret scripts.

--- secrets/moodle-virtuallabx/pull.sh
+++ secrets/moodle-virtuallabx/pull.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl config use-context openuchile
-export MOODLE_NAMESPACE=moodle-ing
-
 # Moodle configuration
-kubectl -n $MOODLE_NAMESPACE get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
+kubectl --context openuchile -n moodle-ing get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
 
 # Moodle dev configuration
-kubectl -n $MOODLE_NAMESPACE get secret moodle-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle.php
+kubectl --context openuchile -n moodle-ing get secret moodle-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle.php
 
 # Postgres config
-kubectl -n $MOODLE_NAMESPACE get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
+kubectl --context openuchile -n moodle-ing get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
 
 # Redis config
-kubectl -n $MOODLE_NAMESPACE get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
+kubectl --context openuchile -n moodle-ing get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
