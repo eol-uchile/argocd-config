d3279a10f33fce254d6207f30a0e7acffa8a5694
fsologureng <fsologur@uchile.cl>
2026-03-03 10:17:54 -0300

    fix(moodle-vlabx): add missing secret pull for prod config

--- secrets/moodle-virtuallabx/pull.sh
+++ secrets/moodle-virtuallabx/pull.sh
@@ -5,10 +5,13 @@ kubectl config use-context openuchile
 export MOODLE_NAMESPACE=moodle-ing
 
 # Moodle configuration
+kubectl -n $MOODLE_NAMESPACE get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
+
+# Moodle dev configuration
 kubectl -n $MOODLE_NAMESPACE get secret moodle-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle.php
 
-# Redis config
+# Postgres config
 kubectl -n $MOODLE_NAMESPACE get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
 
-# Postgres config
+# Redis config
 kubectl -n $MOODLE_NAMESPACE get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
