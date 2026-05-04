851f24a5155d83ce5a18dcdeb932b7ea31a14c12
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-05-04 15:00:05 -0400

    refactor(edx-backup): update secret scripts to use explicit kubectl contexts

--- secrets/edx-backup/pull.sh
+++ secrets/edx-backup/pull.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl config use-context eol
-export EDX_NAMESPACE=edx-backup
+# eol-c
+kubectl --context eol -n edx-backup get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
+kubectl --context eol -n edx-backup get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
+kubectl --context eol -n edx-backup get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
+kubectl --context eol -n edx-backup get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
+kubectl --context eol -n edx-backup get secret redfid-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > redfid-config.env
 
-# configs
-kubectl -n $EDX_NAMESPACE get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
-kubectl -n $EDX_NAMESPACE get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
-kubectl -n $EDX_NAMESPACE get secret redfid-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > redfid-config.env
-kubectl -n $EDX_NAMESPACE get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
-kubectl -n $EDX_NAMESPACE get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
-
-# OEOL
-kubectl config use-context oeol
-kubectl -n $EDX_NAMESPACE get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > oeol-common.env
-kubectl -n $EDX_NAMESPACE get secret umce-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > umce-config.env
+# oeol-c
+kubectl --context oeol -n edx-backup get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > oeol-common.env
+kubectl --context oeol -n edx-backup get secret umce-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > umce-config.env
