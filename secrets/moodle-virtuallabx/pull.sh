ee325c802cab5eeb294aa65563a0410a31316ac4
fsologureng <fsologur@uchile.cl>
2026-05-07 14:35:07 -0400

    refactor(moodle-virtuallabx/dev): re-sealsecrets for oeol-c

--- secrets/moodle-virtuallabx/pull.sh
+++ secrets/moodle-virtuallabx/pull.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
+# Production
 # Moodle configuration
 kubectl --context openuchile -n moodle-ing get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
-
-# Moodle dev configuration
-kubectl --context openuchile -n moodle-ing get secret moodle4-dev-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4-dev.php
 
 # Postgres config
 kubectl --context openuchile -n moodle-ing get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
 
 # Redis config
 kubectl --context openuchile -n moodle-ing get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
+
+# Development
+# Moodle dev configuration
+kubectl --context oeol -n moodle-ing get secret moodle4-dev-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4-dev.php
+
+# Postgres config
+kubectl --context oeol -n moodle-ing get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
+
+# Redis config
+kubectl --context oeol -n moodle-ing get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
