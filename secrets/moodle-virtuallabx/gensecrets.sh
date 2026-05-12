dee8d97291a7f96974a75c702b7e013e8e267dc2
fsologureng <fsologur@uchile.cl>
2026-05-12 13:16:46 -0400

    refactor(moodle-virtuallabx)!: mv moodle to oeol-c
    - Unify applications directory.
    - Unify argocd project & application into oeol cluster.

--- secrets/moodle-virtuallabx/gensecrets.sh
+++ secrets/moodle-virtuallabx/gensecrets.sh
@@ -2,9 +2,9 @@
 set -e
 
 # dev
-kubectl --context oeol -n moodle-ing create secret generic moodle-dev-config --from-file=moodle.php=./moodle-dev.php --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx-oeol/secrets/moodle-dev-config.yaml
-kubectl --context oeol -n moodle-ing create secret generic postgres --from-env-file=./postgres.env  --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx-oeol/secrets/postgres.yaml
-kubectl --context oeol -n moodle-ing create secret generic redis --from-file=redis.conf=./redis.conf --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx-oeol/secrets/redis.yaml
+kubectl --context oeol -n moodle-ing create secret generic moodle-dev-config --from-file=moodle.php=./moodle-dev.php --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx/secrets/moodle-dev-config.yaml
+kubectl --context oeol -n moodle-ing create secret generic postgres --from-env-file=./postgres.env  --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx/secrets/postgres.yaml
+kubectl --context oeol -n moodle-ing create secret generic redis --from-file=redis.conf=./redis.conf --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx/secrets/redis.yaml
 # prod
 kubectl --context oeol -n moodle-ing create secret generic moodle-config --from-file=moodle.php=./moodle.php --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx/secrets/moodle-config.yaml
 kubectl --context oeol -n moodle-ing create secret generic postgres --from-env-file=./postgres.env  --dry-run=client -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../moodle-virtuallabx/secrets/postgres.yaml
