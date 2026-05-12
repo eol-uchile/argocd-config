b68e01718e06a2b5f7fcb6ff97baf2ecde93439f
fsologureng <fsologur@uchile.cl>
2026-05-12 10:52:15 -0400

    refactor(moodle-virtuallabx): re-sealsecrets for oeol-c

--- secrets/moodle-virtuallabx/pull.sh
+++ secrets/moodle-virtuallabx/pull.sh
@@ -3,18 +3,13 @@ set -e
 
 # Production
 # Moodle configuration
-kubectl --context openuchile -n moodle-ing get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
-
-# Postgres config
-kubectl --context openuchile -n moodle-ing get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
-
-# Redis config
-kubectl --context openuchile -n moodle-ing get secret redis -o jsonpath="{.data['redis\.conf']}" | base64 -d > redis.conf
+kubectl --context oeol -n moodle-ing get secret moodle4-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle4.php
 
 # Development
 # Moodle dev configuration
 kubectl --context oeol -n moodle-ing get secret moodle-dev-config -o jsonpath="{.data['moodle\.php']}" | base64 -d > moodle-dev.php
 
+# Common
 # Postgres config
 kubectl --context oeol -n moodle-ing get secret postgres -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > postgres.env
 
