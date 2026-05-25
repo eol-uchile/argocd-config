b0469a37b1a15a4de5b91f6ffb15c857641b3ac1
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-05-25 17:48:43 -0400

    refactor(edx-backup/eol): move files to edx-backup-oeol dir - update sealed secret to oeol context

--- secrets/edx-backup/pull.sh
+++ secrets/edx-backup/pull.sh
@@ -3,11 +3,11 @@ set -e
 
 # eol-c
 kubectl --context eol -n edx-backup get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > common.env
-kubectl --context eol -n edx-backup get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
 kubectl --context eol -n edx-backup get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
 kubectl --context eol -n edx-backup get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
 
 # oeol-c
+kubectl --context oeol -n edx-backup get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
 kubectl --context oeol -n edx-backup get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > oeol-common.env
 kubectl --context oeol -n edx-backup get secret umce-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > umce-config.env
 kubectl --context oeol -n edx-backup get secret redfid-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > redfid-config.env
