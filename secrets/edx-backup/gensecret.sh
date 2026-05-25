81de76c4479f4add5321e5efe97f1087a24e53c8
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-05-25 17:48:43 -0400

    refactor(edx-backup/eol): move files to edx-backup-oeol dir - update sealed secret to oeol context

--- secrets/edx-backup/gensecret.sh
+++ secrets/edx-backup/gensecret.sh
@@ -2,11 +2,11 @@
 
 # eol-c
 kubectl --context eol -n edx-backup create secret generic common-config --dry-run=client --from-env-file=./common.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/common.yaml
-kubectl --context eol -n edx-backup create secret generic eol-config --dry-run=client --from-env-file=./eol-config.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/eol.yaml
 kubectl --context eol -n edx-backup create secret generic openuchile-config --dry-run=client --from-env-file=./openuchile-config.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/openuchile.yaml
 kubectl --context eol -n edx-backup create secret generic virtuallabx-config --dry-run=client --from-env-file=./virtuallabx-config.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/virtuallabx.yaml
 
 # oeol-c
+kubectl --context oeol -n edx-backup create secret generic eol-config --dry-run=client --from-env-file=./eol-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/eol.yaml
 kubectl --context oeol -n edx-backup create secret generic common-config --dry-run=client --from-env-file=./oeol-common.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/common.yaml
 kubectl --context oeol -n edx-backup create secret generic umce-config --dry-run=client --from-env-file=./umce-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/umce.yaml
 kubectl --context oeol -n edx-backup create secret generic redfid-config --dry-run=client --from-env-file=./redfid-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/redfid.yaml
