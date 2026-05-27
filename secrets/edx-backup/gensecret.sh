29aea85e8582defdeba1d2456aca95f6173d4805
Vicente Astorga <vicente.astorga.dev@gmail.com>
2026-05-27 10:56:38 -0400

    refactor(edx-backup/virtuallabx): move files to edx-backup-oeol dir - update sealed secret to oeol context

--- secrets/edx-backup/gensecret.sh
+++ secrets/edx-backup/gensecret.sh
@@ -2,9 +2,9 @@
 
 # eol-c
 kubectl --context eol -n edx-backup create secret generic common-config --dry-run=client --from-env-file=./common.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/common.yaml
-kubectl --context eol -n edx-backup create secret generic virtuallabx-config --dry-run=client --from-env-file=./virtuallabx-config.env -o yaml | kubeseal --context eol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/virtuallabx.yaml
 
 # oeol-c
+kubectl --context oeol -n edx-backup create secret generic virtuallabx-config --dry-run=client --from-env-file=./virtuallabx-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/virtuallabx.yaml
 kubectl --context oeol -n edx-backup create secret generic openuchile-config --dry-run=client --from-env-file=./openuchile-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/openuchile.yaml
 kubectl --context oeol -n edx-backup create secret generic eol-config --dry-run=client --from-env-file=./eol-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/eol.yaml
 kubectl --context oeol -n edx-backup create secret generic common-config --dry-run=client --from-env-file=./oeol-common.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup-oeol/secrets/common.yaml
