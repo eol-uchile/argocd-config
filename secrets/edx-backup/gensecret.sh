e176b9565548047ad50bcfb50447ff234587571d
fsologureng <fsologur@uchile.cl>
2026-06-18 10:34:07 -0400

    refactor(edx-backup): rename virtuallabx as cla
    Rename job, pv, pvc, working directory, manifests, secret name & `PLATFORM_NAME` inside config secret.

--- secrets/edx-backup/gensecret.sh
+++ secrets/edx-backup/gensecret.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl --context oeol -n edx-backup create secret generic virtuallabx-config --dry-run=client --from-env-file=./virtuallabx-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/virtuallabx.yaml
+kubectl --context oeol -n edx-backup create secret generic cla-config --dry-run=client --from-env-file=./cla-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/cla.yaml
 kubectl --context oeol -n edx-backup create secret generic openuchile-config --dry-run=client --from-env-file=./openuchile-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/openuchile.yaml
 kubectl --context oeol -n edx-backup create secret generic eol-config --dry-run=client --from-env-file=./eol-config.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/eol.yaml
 kubectl --context oeol -n edx-backup create secret generic common-config --dry-run=client --from-env-file=./oeol-common.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../edx-backup/secrets/common.yaml
--- secrets/edx-backup/pull.sh
+++ secrets/edx-backup/pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-kubectl --context oeol -n edx-backup get secret virtuallabx-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > virtuallabx-config.env
+kubectl --context oeol -n edx-backup get secret cla-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > cla-config.env
 kubectl --context oeol -n edx-backup get secret openuchile-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > openuchile-config.env
 kubectl --context oeol -n edx-backup get secret eol-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > eol-config.env
 kubectl --context oeol -n edx-backup get secret common-config -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > oeol-common.env
