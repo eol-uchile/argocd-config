7471ebe4cceb3f20ccbdbc0a711e33aa6a8aa12c
Vicente Astorga <vicente.astorga@uchile.cl>
2026-07-29 10:21:18 -0400

    chore(argocd-config): remove resources & secret

--- secrets/argocd-ingress/gensecret.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl config use-context oeol
-
-kubectl create secret generic basic-auth --dry-run=client --from-file=auth=./basic_auth -o yaml | kubeseal --scope cluster-wide --controller-namespace sealed-secrets -o yaml > ../../argocd-ingress/secret.yaml
--- secrets/argocd-ingress/pull.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-kubectl config use-context oeol
-export EDX_NAMESPACE=argocd
-
-# configs
-kubectl -n $EDX_NAMESPACE get secret basic-auth -o jsonpath="{.data['auth']}" | base64 -d > basic_auth
