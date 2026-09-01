7f052150d45004ec590263a17ed77be4cf950f6b
Vicente Astorga <vicente.astorga@uchile.cl>
2026-09-01 16:52:56 -0400

    chore(metabase): migrate ingress to gateway w/listenerset - update project resources white list - add HTTPRoute & ListenerSet to replace ingress resource & redirect http to https - update secret & secret generation scripts to define key .htpasswd

--- secrets/metabase/gensecret.sh
+++ secrets/metabase/gensecret.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 kubectl --context oeol -n metabase create secret generic metabase --dry-run=client --from-env-file=./metabase.env -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../metabase/secrets.yaml
-kubectl --context oeol -n metabase create secret generic basic-auth --dry-run=client --from-file=auth=./basic_auth -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../metabase/access.yaml
+kubectl --context oeol -n metabase create secret generic basic-auth --dry-run=client --from-file=.htpasswd=./basic_auth -o yaml | kubeseal --context oeol --controller-namespace sealed-secrets -o yaml > ../../metabase/access.yaml
