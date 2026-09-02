13d6aa8ea39ae0240d0fa111d6df5373378450a7
Vicente Astorga <vicente.astorga@uchile.cl>
2026-09-02 10:11:17 -0400

    fix(metabase): cypher secret in SHA - update pull .sh secret script

--- secrets/metabase/pull.sh
+++ secrets/metabase/pull.sh
@@ -3,4 +3,4 @@ set -e
 
 # configs
 kubectl --context oeol -n metabase get secret metabase -o json | jq -r '.data | keys[] as $k | "\($k)=\(.[$k] | @base64d)"' > metabase.env
-kubectl --context oeol -n metabase get secret basic-auth -o jsonpath="{.data['.htpasswd']}" | base64 -d > basic_auth
+kubectl --context oeol -n metabase get secret basic-auth -o jsonpath="{.data['\.htpasswd']}" | base64 -d > basic_auth
